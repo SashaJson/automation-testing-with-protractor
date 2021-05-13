6d8d27c51ab5e9bbb55c58297c2b62c0dfbd6a68
SashaJson <sashamiller666@gmail.com>
2021-05-14 00:34:53 +0300

    added vote for movies

--- superhero/js/script.js
+++ superhero/js/script.js
@@ -9,3 +9,28 @@ $('#addHero-form').submit(function (event) {
     }
     event.preventDefault();
 });
+
+$('#vote-form').submit(function (event) {
+    var movie1Votes = $('#movieVotes1').text();
+    var movie2Votes = $('#movieVotes2').text();
+    var movie3Votes = $('#movieVotes3').text();
+    var movie4Votes = $('#movieVotes4').text();
+    var movie5Votes = $('#movieVotes5').text();
+    if($('#heroMovieRadio1').is(':checked')) {
+        movie1Votes = parseInt(movie1Votes) + 1;
+        $('#movieVotes1').text(movie1Votes);
+    } else if($('#heroMovieRadio2').is(':checked')) {
+        movie2Votes = parseInt(movie2Votes) + 1;
+        $('#movieVotes2').text(movie2Votes);
+    } else if($('#heroMovieRadio3').is(':checked')) {
+        movie3Votes = parseInt(movie3Votes) + 1;
+        $('#movieVotes3').text(movie3Votes);
+    } else if($('#heroMovieRadio4').is(':checked')) {
+        movie4Votes = parseInt(movie4Votes) + 1;
+        $('#movieVotes4').text(movie4Votes);
+    } else if($('#heroMovieRadio5').is(':checked')) {
+        movie5Votes = parseInt(movie5Votes) + 1;
+        $('#movieVotes5').text(movie5Votes);
+    }
+    event.preventDefault();
+});
