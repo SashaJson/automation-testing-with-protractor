62702f0fda6f7990af206a329409fa5cde528ce7
SashaJson <sashamiller666@gmail.com>
2021-05-25 23:27:29 +0300

    added test "login.spec.js"

--- tests/suits/login.spec.js
+++ tests/suits/login.spec.js
@@ -1,7 +1,16 @@
+
+const LoginPage = require('../pages/loginPage');
+
 describe('Login page Tests', () => {
 
-    beforeEach(() => {
+    let loginPage;
 
+    beforeEach(() => {
+        loginPage = new LoginPage();
+        // Since this is not an Angular Site
+        browser.ignoreSynchronization = true;
+        // Open URL
+        browser.get('../../superhero/index.html');
     });
 
     afterEach(() => {
