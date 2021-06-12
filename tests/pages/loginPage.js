4467d63630dd9dfa1430ec8d792c9c8832ae71ab
SashaJson <sashamiller666@gmail.com>
2021-06-12 18:29:28 +0300

    added method to proto login page

--- tests/pages/loginPage.js
+++ tests/pages/loginPage.js
@@ -1,7 +1,14 @@
-const LoginPage = () => {};
+const LoginPage = function () {};
 
 LoginPage.prototype = Object.create({}, {
-    elementName: { get: () => element(by.id('')) };
+    loginTitleTxt: { get: () => element(by.id('login-title')) },
+    emailFildLabel: { get: () => element(by.css('#form-login > div:nth-child(1) > label')) },
+    emailFild: { get: () => element(by.id('loginEmail')) },
+    passwordFildLabel: { get: () => element(by.css('#form-login > div:nth-child(2) > label')) },
+    passwordFild: { get: () => element(by.id('loginPassword')) },
+    rememberCheckLabel: { get: () => element(by.css('#form-login > div.form-check > label')) },
+    rememberCheck: { get: () => element(by.id('rememberLoginCheck')) },
+    submitButton: { get: () => element(by.buttonText('Submit')) }
 });
 
 module.exports = LoginPage;
