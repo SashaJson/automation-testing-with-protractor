05a800a0244a01abe075fdfec2c2221859f9b627
SashaJson <sashamiller666@gmail.com>
2021-06-25 23:25:06 +0300

    updated Roster page object and suit

--- tests/pages/rosterPage.js
+++ tests/pages/rosterPage.js
@@ -1,3 +1,17 @@
 'use strict';
 
 const RosterPage = function () {};
+
+RosterPage.prototype = Object.create({}, {
+    rosterTitleText: { get: () => element(by.css('body > div.container-fluid > div:nth-child(4) > h3')) },
+    wolverineItemText: { get: () => element(by.css('#hero-list > li:nth-child(1)')) },
+    ironManItemText: { get: () => element(by.css('#hero-list > li:nth-child(2)')) },
+    deadpoolItemText: { get: () => element(by.css('#hero-list > li:nth-child(3)')) },
+    thorItemText: { get: () => element(by.css('#hero-list > li:nth-child(4)')) },
+    spiderMamItemText: { get: () => element(by.css('#hero-list > li:nth-child(5)')) },
+    rosterFieldLabel: { get: () => element(by.css('#addHero-form > div > label')) },
+    rosterField: { get: () => element(by.id('heroInput')) },
+    submitButton: { get: () => element(by.css('#addHero-form > button')) }
+});
+
+module.exports = RosterPage;
--- tests/suits/roster.spec.js
+++ tests/suits/roster.spec.js
@@ -1,3 +1,40 @@
 'use strict';
 
 const RosterPage = require('../pages/rosterPage');
+
+describe('Roster Page', () => {
+
+    let rosterPage;
+
+    beforeEach(() => {
+        rosterPage = new RosterPage();
+        // Since this is not an Angular Site
+        browser.ignoreSynchronization = true;
+        browser.get('../../superhero/index.html');
+    });
+
+    afterEach(() => {
+
+    });
+
+    it('', () => {
+
+    });
+
+    it('', () => {
+
+    });
+
+    it('', () => {
+
+    });
+
+    it('', () => {
+
+    });
+
+    it('', () => {
+
+    });
+
+});
