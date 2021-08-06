8b47919615bd98f694a73bb25ae39b58d7584979
SashaJson <sashamiller666@gmail.com>
2021-08-06 21:18:31 +0300

    init header page and suit

--- tests/pages/headerPage.js
+++ tests/pages/headerPage.js
@@ -1,3 +1,11 @@
 'use strict';
 
 const HeaderPage = function () {};
+
+HeaderPage.prototype = Object.create({}, {
+    headerTitle: { get: () => element(by.css('body > div.container-fluid > div:nth-child(1) > h1')) },
+    headerImage: { get: () => element(by.css('body > div.container-fluid > div:nth-child(2) > img')) },
+    headerBody: { get: () => element(by.css('body > div.container-fluid > div:nth-child(3) > div > p')) }
+});
+
+module.exports = HeaderPage;
--- tests/suits/header.spec.js
+++ tests/suits/header.spec.js
@@ -1,3 +1,30 @@
 'use strict';
 
 const HeaderPage = require('../pages/headerPage');
+
+describe('Header Page', () => {
+    let headerPage;
+
+    beforeEach(() => {
+        headerPage = new HeaderPage();
+        browser.ignoreSynchronization = true;
+        browser.get('../../superhero/index.html');
+    });
+
+    afterEach(() => {
+
+    });
+
+    it('should contain all Header elements', () => {
+
+        expect(headerPage.headerTitle.isDisplayed()).toBe(true);
+        expect(headerPage.headerTitle.getText()).toBe('Superhero Roster');
+
+        expect(headerPage.headerImage.isDisplayed()).toBe(true);
+
+        expect(headerPage.headerBody.isDisplayed()).toBe(true);
+        expect(headerPage.headerBody.getText()).toBe('Image that you are tasked with building a team of Superhero to save the world. We have given you a few heroes to start with. Add as many heroes as you would like to round out your dream team.');
+
+    });
+
+});
