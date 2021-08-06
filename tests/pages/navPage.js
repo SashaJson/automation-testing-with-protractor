d27f347ce9ad7b243bc535aa79a28cad2aa8ed5e
SashaJson <sashamiller666@gmail.com>
2021-08-06 21:10:13 +0300

    init nav page and suit

--- tests/pages/navPage.js
+++ tests/pages/navPage.js
@@ -1,3 +1,9 @@
 'use strict';
 
 const NavPage = function () {};
+
+NavPage.prototype = Object.create({}, {
+    sample: { get: () => element(by.id('')) }
+});
+
+module.exports = NavPage;
--- tests/suits/nav.spec.js
+++ tests/suits/nav.spec.js
@@ -1,3 +1,22 @@
 'use strict';
 
 const NavPage = require('../pages/navPage');
+
+describe('', () => {
+   let navPage;
+
+    beforeEach(() => {
+        navPage = new NavPage();
+        browser.ignoreSynchronization = true;
+        browser.get('../../superhero/index.html');
+    });
+
+    afterEach(() => {
+
+    });
+
+    it('', () => {
+
+    });
+
+});
